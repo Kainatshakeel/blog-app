a60a4695ab5ff8686f4987476c1a7144389f75b1
Kainatshakeel <kainatshakeel447@gmail.com>
2024-03-01 14:35:52 +0500

    create and add footer component

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -49,7 +49,7 @@ export default function SignUp() {
             App
           </Link>
           <p className='text-sm mt-5'>
-            This is a demo project. You can sign up with your email and password
+             Sign up with your email and password
             or with Google.
           </p>
         </div>
